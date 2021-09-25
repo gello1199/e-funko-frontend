f05347729271ec76caa42b0e64933ead9b75833e
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-09-25 17:59:00 -0400

    Added getStores method

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ const itemCall = new ItemService(port)
 
 categoryCall.getCategories();
 itemCall.getItems();
+storeCall.getStores();
--- src/storeService.js
+++ src/storeService.js
@@ -2,4 +2,10 @@ class StoreService {
     constructor(port) {
         this.port = port
     }
+
+    getStores() {
+        fetch(this.port + `/stores`)
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+    }
 }
