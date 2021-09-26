fdd5422ea7b3b90a1211aa9b0003e511fba50a3d
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-09-25 20:11:32 -0400

    Store constructor added

---
+++ src/store.js
@@ -0,0 +1,8 @@
+class Store {
+    constructor({name, id, location, phone_number}) {
+        this.name = name;
+        this.id = id;
+        this.location = location;
+        this.phone_number = phone_number;
+    }
+}
