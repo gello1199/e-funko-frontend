feef7c5dc49c24d6f0fdad0e1f428ffb8edfcb04
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-09-25 20:10:25 -0400

    Item constructor added

---
+++ src/item.js
@@ -0,0 +1,11 @@
+class Item {
+    constructor({name, id, price, description, image, category_id}) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+        this.description = description;
+        this.image = image;
+        this.category_id = category_id;
+
+    }
+}
