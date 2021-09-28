77a0ab506d0b9f8851cb8da0aaa618d4bcc9f4de
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-09-28 14:19:01 -0400

    Added form and dropdown variables for form

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const port = `http://localhost:3000`
 const categoryCall = new CategoryService(port) // an instance to call methods on
 const storeCall = new StoreService(port)
 const itemCall = new ItemService(port)
+const form = document.getElementById("item-form")
+const dropDown = document.getElementById("category-dropdown")
 
 
 // categoryCall.getCategories();
