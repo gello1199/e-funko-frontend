2968cd5297c2ce5d602a6354fdd1a408113f9587
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-09-28 10:37:59 -0400

    Added css to center and border images

--- src/item.js
+++ src/item.js
@@ -17,11 +17,14 @@ class Item {
 
     renderToLi() {
         this.element.innerHTML = `
-        <div data-id="${this.id}">
-        <h3 class="name">${this.name}</h3>
-        <p class="price">$ ${this.price}</p>
-        <p class="description">${this.description}</p>
-        <img src="${this.image}" class="image">
+        <div class="id" data-id="${this.id}">
+        <h3 class="name">Name: ${this.name}</h3>
+        <p class="price">Price: $ ${this.price}</p>
+        <p class="description">Description: ${this.description}</p>
+        <img src="${this.image}" class="image"> <br><br>
+        <button class="edit">Edit</button>
+        <button class="delete">Delete</button>
+
         </div>
         `
         return this.element
