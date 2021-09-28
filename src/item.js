1253727af04f18c3ea75f0422d225899e1883106
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-09-28 09:52:58 -0400

    Added static all and render function to item

--- src/item.js
+++ src/item.js
@@ -1,4 +1,5 @@
 class Item {
+    static all = [];
     constructor({name, id, price, description, image, category_id}) {
         this.name = name;
         this.id = id;
@@ -9,7 +10,20 @@ class Item {
         this.element = document.createElement('li');
         this.element.dataset['id'] = id;
         this.element.id = `item-${id}`;
-        debugger
+        Item.all.push(this)
+        // debugger
     }
 
+    renderToLi() {
+        this.element.innerHTML = `
+        <div data-id="${this.id}">
+        <h3 class="name">${this.name}</h3>
+        <p class="price">${this.price}</p>
+        <p class="description">${this.description}</p>
+        <p class="image">${this.image}</p>
+        </div>
+        `
+        return this.element
+    }
+    
 }
