b1fb6e91fa36d7f732530ba057b277ae219cace6
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-09-30 17:18:20 -0400

    Added eventlistener to overlay

--- index.js
+++ index.js
@@ -38,3 +38,8 @@ closeModal.addEventListener(`click`, function(){
     modal.classList.add('hidden')
     overlay.classList.add('hidden')
 })
+
+overlay.addEventListener(`click`, function() {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+})
